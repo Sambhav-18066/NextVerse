22dd6e5056a923f959541e73b8d3674f241cb81b
SAMBHAV BARIK <sambhav.barik@cutm.ac.in>
2025-10-30 16:05:09 +0000

    Try fixing this error: `Runtime Error: Error: Service account must be an

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,3 +1,4 @@
+
 // IMPORTANT: This file should only be used in server-side code.
 // It is not intended for use in client-side code.
 import { initializeApp, getApps, App, cert } from 'firebase-admin/app';
@@ -6,9 +7,11 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 let app: App;
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : undefined;
+// The service account is automatically discovered by App Hosting.
+// No need to manually parse environment variables.
+// const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+//   ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+//   : undefined;
 
 function getApp() {
   if (app) {
@@ -17,9 +20,9 @@ function getApp() {
   if (getApps().length) {
     app = getApps()[0];
   } else {
-    app = initializeApp({
-        credential: cert(serviceAccount!),
-    });
+    // When deployed to App Hosting, initializeApp() automatically discovers
+    // the project's credentials.
+    app = initializeApp();
   }
   return app;
 }
