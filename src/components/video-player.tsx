50d6d76b4a4a849f4cfe7aa5953a33964d942bba
SAMBHAV BARIK <sambhav.barik@cutm.ac.in>
2025-10-30 10:43:02 +0000

    in the watch page www-embed-player-pc.js:2844
     GET https://static.dou

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -7,7 +7,7 @@ export function VideoPlayer({ youtubeId }: VideoPlayerProps) {
     <div className="aspect-video w-full bg-black rounded-lg overflow-hidden shadow-2xl">
       <iframe
         className="w-full h-full"
-        src={`https://www.youtube.com/embed/${youtubeId}?autoplay=0&rel=0&modestbranding=1`}
+        src={`https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=0&rel=0&modestbranding=1`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
