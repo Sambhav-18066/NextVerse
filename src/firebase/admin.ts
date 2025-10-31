cf3c40e7a05a03c479b84cda967584a0effc25c2
SAMBHAV BARIK <sambhav.barik@cutm.ac.in>
2025-10-31 04:50:31 +0000

    same error

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,6 +1,7 @@
 
 // IMPORTANT: This file should only be used in server-side code.
 // It is not intended for use in client-side code.
+import 'dotenv/config';
 import { initializeApp, getApps, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
